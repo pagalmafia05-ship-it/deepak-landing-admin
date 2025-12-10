cfa8e603481e0b2582f86dc4a7fd382c5194bf5f
pagalmafia05-ship-it <pagalmafia05@gmail.com>
2025-12-10 18:48:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import AdminPanel from './AdminPanel'
-import './styles.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";   // extension bhi add kar de
+import "./styles.css";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/admin" element={<AdminPanel />} />
-    </Routes>
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
